112b02a149145d5385587d40a54df34f7f1123aa
xXVikanXx <hotdogmannen2@gmail.com>
2025-04-05 19:41:33 +0200

    Ghost boat animated

--- Scripts/GhostShip.cs
+++ Scripts/GhostShip.cs
@@ -4,13 +4,26 @@ using Godot;
 public partial class GhostShip : Node3D
 {
 	[Export] private float _positionDifference = 0.1f;
+	[Export] public float oarSpeed {get; set;}
+
+	[ExportCategory("Animation")]
+	[Export] public float BobbingSpeed {get; set;}
+    [Export] public float BobbingVelocitySpeedupPercentage {get; set;}
+    [Export] public float BobbingMagnitude {get; set;}
+	[Export] public float OarAnimationHeight {get; set;}
+    [Export] public float OarAnimationWidth {get; set;}
+    [Export] public float OarAnimationSharpness {get; set;}
+
+	[ExportCategory("Oars")]
+	[Export] Node3D oarLeft { get; set; }
+    [Export] Node3D oarRight { get; set; }
 
 	private List<Vector3> _playerPositions;
 	private int _pathIndex = 1;
 	private bool _canMove = false;
 	private float _currentTime = 0.0f;
 	private float _timeBetweenSaves = 0.0f;
-
+	private double animationTime;
     public void Init(List<Vector3> positions, float timeBetweenSaves)
 	{
 		if(positions == null)
@@ -26,11 +39,15 @@ public partial class GhostShip : Node3D
 	{
 		if(_playerPositions == null)
 			return;
+		
 		_canMove = true;
 	}
 
     public override void _Process(double delta)
     {
+		ApplyBobbingAnimation(delta);
+		UpdateOarAnimation(delta);
+
 		if(!_canMove)
 			return;
 
@@ -54,4 +71,48 @@ public partial class GhostShip : Node3D
 
 		Position = newPos;
 	}
+
+	private void UpdateOarAnimation(double delta)
+    {
+		SetOarRotation(delta, oarLeft, _currentTime * oarSpeed);
+		SetOarRotation(delta, oarRight, _currentTime * oarSpeed, true);
+
+        // if(leftOarPower > margin && leftOarPower <= 1f){
+        //     SetOarRotation(delta, oarLeft,leftOarPower);
+        // }
+        // else{
+        //     SetOarRotation(delta, oarLeft,0);
+        // }
+        // if(rightOarPower > margin && rightOarPower <= 1f){
+        //     SetOarRotation(delta, oarRight,rightOarPower, true);
+        // }
+        // else{
+        //     SetOarRotation(delta, oarRight,0, true);
+            
+        // }
+    }
+
+	private void SetOarRotation(double delta,Node3D oar, float power,bool flipped = false){
+        var target = new Vector3(
+            0.1f,
+            (float)((flipped? -1 : 1)*Mathf.Cos(power*2*Mathf.Pi) * OarAnimationWidth),
+            (float)((flipped? 1 : -1)*Mathf.Sin(power*2*Mathf.Pi) * OarAnimationHeight));
+
+        float weight = 1f - Mathf.Exp(-OarAnimationSharpness * (float)delta);
+        oar.RotationDegrees = oar.RotationDegrees.Lerp(target,weight);
+
+    }
+
+	private void ApplyBobbingAnimation(double delta)
+	{    
+        animationTime += delta;
+
+        var target = new Vector3(
+            (float)Mathf.Cos(animationTime*BobbingSpeed) * BobbingMagnitude,
+            0f,
+            (float)Mathf.Sin(animationTime*BobbingSpeed+0.2f) * BobbingMagnitude);
+        RotationDegrees = target;
+    }
+
+	public static float Fade(float t) => t * t * t * (t * (t * 6 - 15) + 10);   
 }
