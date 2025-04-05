6716ed643c8ed89a9e9829bb4e846cfa6d3e8f85
Vide <vide.franden@gmail.com>
2025-04-05 23:22:22 +0200

    stuff

--- Scenes/Levels/PathInstantiate.cs
+++ Scenes/Levels/PathInstantiate.cs
@@ -8,11 +8,12 @@ public partial class PathInstantiate : Path3D
 
     public override void _Ready()
     {
+        if(spawnSpacing == 0) return;
         base._Ready();
         float length = Curve.GetBakedLength();
         GD.Print(length);
         for(float t = 0; t < length; t+= spawnSpacing){
-            Vector3 samplePos = Curve.SampleBaked(t);
+            Vector3 samplePos = Curve.SampleBaked(t,true);
             var node = SceneToInstatiate.Instantiate() as Node3D;
             AddChild(node);
             node.Position = samplePos;
